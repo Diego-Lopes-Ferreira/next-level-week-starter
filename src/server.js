587b380bd49928971f49f5c209a1c1600536d56a
Diego-Lopes-Ferreira <diego.lopes.ferreira.br@gmail.com>
2020-06-04 11:37:16 -0300

    add nunjucks

--- src/server.js
+++ src/server.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
+
+
 const server = express();
+server.use(express.static('public'))
+nunjucks.configure('src/views', {express: server, noCache: true});
+
+//Routes
+server.get('/', (req, res) => {
+  res.render('index.html')
+});
+server.get('/create', (req, res) => {
+  res.render('create.html')
+});
+server.get('/results', (req, res) => {
+  res.render('results.html')
+});
 
 server.listen('3000');
-server.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/')
-});
